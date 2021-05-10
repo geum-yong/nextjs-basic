ca722d4d51884a75da46dc96eaa5529ee493af29
geum-yong <gmyong21@gmail.com>
2021-05-10 15:19:37 +0900

    20210510 함수 라우팅

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,28 @@
 // '/' 경로를 가지는 페이지
-import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const router = useRouter();
+
   return (
     <div>
-      <h2>Link to 'potato' Page</h2>
-      <Link href='/vegetable/potato'>
-        <a>Move to '/vegetable/potato'</a>
-      </Link>
+      <button type='button' onClick={() => router.push('/tomato')}>
+        tomato로 가기
+      </button>
+      <p>이름</p>
+      <input
+        type='text'
+        value={name}
+        onChange={e => setName(e.target.value)}
+        style={{ marginTop: '12px' }}
+      />
+      <button type='button' onClick={() => router.push(`/vegetable/${name}`)}>
+        {name}으로 가기
+      </button>
     </div>
   );
 };
+
 export default App;
